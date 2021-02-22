016f0fe48998242e764c3b8dbc85641c680d438a
André Oliveira <andrev-eng@ufmg.br>
2021-02-22 15:53:20 -0300

    Primeiro commit.

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (request, response) => {
+    response.json({'message': 'Hello'});
+});
+
+app.listen(3000, () => console.log('Server is running!'));
