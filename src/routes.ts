a1faeb5b82d047946466b12319bcac6cf5ca381f
André Oliveira <andrev-eng@ufmg.br>
2021-02-25 17:28:46 -0300

    Implementando SQLite e creiação dos testes.

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,15 @@
 import { Router } from 'express';
+import { SurveyController } from './controllers/SurveyController';
 import { UserController } from './controllers/UserController';
 
 const router = Router();
 
 const userController = new UserController();
+const surveyController = new SurveyController();
 
 router.post('/Users', userController.create);
+
+router.post('/Surveys', surveyController.create);
+router.get('/Surveys', surveyController.show);
 
 export { router };
